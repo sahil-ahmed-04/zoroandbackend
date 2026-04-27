7d6187f77a03afd1894c1b0639a053139cead533
Hesd-Hunter <hesdhunter7@gmail.com>
2026-04-27 19:22:30 +0530

    utility code handled api errors handled

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,32 @@
-const asyncHandler = () => {
+const asyncHandler = (requestHandler) => {
+    (req, res, next)=> {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err)=>next(err))
+    }
 
 }
 
 
 export {asyncHandler}
+
+
+
+// This ()=>{()=>{}} can also be written as ()=>()=>{}
+// const asyncHandler = () => {}
+// const asyncHandler = (func)=>{()=>{}} 
+// const asyncHandler = (func)=>{async()=>{}}
+
+
+ //TRY CATCH METHOD   
+
+// const asyncHandler= (fn) => async(req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+        
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
